a78f1c8a8381c9f224032207a6cd6cc58f21bed9
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-06-30 14:45:44 -0300

    Working

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 const db = mysql.createConnection({
   host: "localhost",
